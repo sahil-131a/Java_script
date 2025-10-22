72c3963c659165b295be4a362dad8bb989060862
sahil-131a <sahilkharbanda131@gmail.com>
2025-10-22 15:15:57 +0530

    learn variable .

--- Day_1/script.js
+++ Day_1/script.js
@@ -1,5 +1,15 @@
 let js = 'new';
-if(js === 'new') alert('keep going on.');
 
-80 + 20 + 5;
 console.log(80 + 20 + 5);
+let firstName = "sahil"; // this is how the variable is declared in the js 
+// we use camelcase to give variable name 
+// we can not start the variable name with capital letter bcz of oop 
+// we can not start the variable name with special char except _ , $ 
+// if we write whole variable with the capital words then js will consider it as the constant
+console.log(firstName);
+let PI = 3.14;
+
+
+
+
+// we must have to write the variable name very discriptive so that it will be easy to read 
