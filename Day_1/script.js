4e35975ce65a3ecb0e14f7828b9e395185ca7eb6
sahil-131a <sahilkharbanda131@gmail.com>
2025-10-22 16:06:35 +0530

    commenting out the js of variable and adding js for data-types.

--- Day_1/script.js
+++ Day_1/script.js
@@ -1,3 +1,8 @@
+/*
+================================================================================
+The code which is commented out is of the variable i have learned through this .
+================================================================================
+
 let js = 'new';
 
 console.log(80 + 20 + 5);
@@ -13,3 +18,19 @@ let PI = 3.14;
 
 
 // we must have to write the variable name very discriptive so that it will be easy to read 
+*/
+
+let js = true;
+
+console.log(js);
+console.log(typeof true); // this will show that value is having the type  not variable 
+console.log(typeof 23);
+console.log(typeof "sahil");
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+age = 12;
+console.log(typeof age);
+console.log(age);
