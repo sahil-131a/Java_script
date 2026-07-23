53c842d8dd1e110b2a13efb5bf2626148da58194
sahil-131a <sahilkharbanda131@gmail.com>
2026-07-23 23:44:18 +0530

    making the cart quantity interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -83,5 +83,14 @@ document.querySelectorAll('.js-add-to-cart-button')
           quantity: 1
         });
       }
-    })
-  })   
+      
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+    });
+  });   
