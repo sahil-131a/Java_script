b033322e4c540b95dca09f447ad610317d2363fa
sahil-131a <sahilkharbanda131@gmail.com>
2026-08-02 19:33:53 +0530

    learn promises and Asynchronous code to promises

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -3,8 +3,53 @@ import {renderPaymentSummary} from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js'
 //import "../data/backend-practice.js"
+import { loadCart } from "../data/cart.js";
 
+
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve();
+        });
+    }),
+
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+]).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    })
+
+}).then(() => {
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+
+/*
 loadProducts(() => {
     renderOrderSummary();
     renderPaymentSummary();
 })
+*/
+
